de3cd6b8484e34f66f45af56091d09361c4f3909
kusuma <yudhaputra5797@gmail.com>
2024-06-12 17:12:17 +0800

    Authentication up until installing lucia

---
+++ actions/auth-actions.js
@@ -0,0 +1,43 @@
+'use server';
+
+import { hashUserPassword } from "@/lib/hash";
+import { CreateUser } from "@/lib/user";
+import { redirect } from "next/navigation";
+
+export async function SignUp(prevState, formData){
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    let errors = {};
+
+    if(!email.includes('@')){
+        errors.email = 'Please enter a valid email address.';
+    }
+
+    if(password.trim().length < 8){
+        errors.password = 'Password must be at least 8 characters long.';
+    }
+
+    if(Object.keys(errors).length > 0)
+    {
+        return {
+            errors,
+        }
+    }
+
+    const hashedPassword = hashUserPassword(password);
+    try {
+        CreateUser(email, hashedPassword);
+    } catch(error){
+        if(error.code == 'SQLITE_CONSTRAINT_UNIQUE'){
+            return {
+                errors: {
+                    email: 'It seems like an account for the chosen email already exists.'
+                }
+            }
+        }
+        throw error;
+    }
+    
+    redirect('/training');
+}
